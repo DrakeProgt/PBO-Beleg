b603be155a867c97309fc3918f17109e4411d17e
DrakeProgt <nils.ziem@web.de>
2018-01-28 04:18:26 +0100

    aktualisiert

--- src/App.vue
+++ src/App.vue
@@ -1,171 +1,46 @@
 <template>
-    <div>
-        <!-- Navbar -->
-        <b-navbar variant="dark" type="dark">
-            <b-container>
-                <b-navbar-brand><img src="Dateien/logo.png" class="d-inline-block align-top"> PBO-Beleg</b-navbar-brand>
-            </b-container>
-        </b-navbar>
+  <div>
+    <!-- Navbar -->
+    <b-navbar variant="dark" type="dark">
+      <b-container>
+        <b-navbar-brand><img src="Dateien/logo.png" class="d-inline-block align-top"> PBO-Beleg</b-navbar-brand>
+      </b-container>
+    </b-navbar>
 
-        <b-container>
-          <!-- User Interface controls -->
-          <b-row>
-            <b-col md="6" class="my-4">
-              <b-form-group horizontal label="Suche:">
-                <b-input-group>
-                  <b-form-input v-model="filter" placeholder="Name oder Alter" />
-                  <b-input-group-button>
-                    <b-btn :disabled="!filter" @click="filter = ''" variant="outline-danger">säubern</b-btn>
-                  </b-input-group-button>
-                </b-input-group>
-              </b-form-group>
-            </b-col>
-            <b-col md="6" class="my-4">
-              <b-form-group horizontal label="Sortierung:">
-                <b-input-group>
-                  <b-form-select v-model="sortBy" :options="sortOptions">
-                    <option slot="first" :value="null">keine</option>
-                  </b-form-select>
-                  <b-input-group-button>
-                    <b-form-select :disabled="!sortBy" v-model="sortDesc">
-                      <option :value="false">Aufwärts</option>
-                      <option :value="true">Abwärts</option>
-                    </b-form-select>
-                  </b-input-group-button>
-                </b-input-group>
-              </b-form-group>
-            </b-col>
-            <b-col md="6" class="my-2">
-              <b-form-group horizontal label="Seite:">              
-                <b-pagination :total-rows="totalRows" :per-page="perPage" v-model="currentPage" />
-                <b-form-checkbox v-model="striped">gestreifte Zeilen</b-form-checkbox>
-              </b-form-group>
-            </b-col>
-            <b-col md="6" class="my-2">
-              <b-form-group horizontal label="Treffer pro Seite:">
-                <b-form-select :options="pageOptions" v-model="perPage" />
-              </b-form-group>
-            </b-col>
-          </b-row>
-
-    <!-- Main table element -->
-    <b-table show-empty
-             stacked="md"
-             :items="items"
-             :fields="fields"
-             :current-page="currentPage"
-             :per-page="perPage"
-             :filter="filter"
-             :sort-by.sync="sortBy"
-             :sort-desc.sync="sortDesc"
-             @filtered="onFiltered"
-             :striped="striped"
-    >
-      <template slot="name" slot-scope="row">{{row.value.first}} {{row.value.last}}</template>
-      <template slot="isActive" slot-scope="row">{{row.value?'Yes :)':'No :('}}</template>
-      <template slot="actions" slot-scope="row">
-        <!-- We use @click.stop here to prevent a 'row-clicked' event from also happening -->
-        <b-button v-b-tooltip.hover title="Zeigt Struktur in JSON-Form" size="sm" @click.stop="info(row.item, row.index, $event.target)" class="mr-3">
-          JSON
-        </b-button>
-        <b-button v-b-tooltip.hover title="Zeigt Informationen in einer Liste an" size="sm" @click.stop="row.toggleDetails">
-          Liste {{ row.detailsShowing ? 'Zuklappen' : 'Ausklappen' }}
-        </b-button>
-      </template>
-      <template slot="row-details" slot-scope="row">
+    <b-container>
+      <b-btn class="btn-block mt-5" :variant=p v-b-toggle.collapse1 @click.stop="details=!details; p=p=='primary'?'success':'primary'">Steakholder {{ !details ? 'Zuklappen' : 'Ausklappen' }}</b-btn>
+      <b-collapse id="collapse1" class="mt-3">
         <b-card>
-          <ul>
-            <li v-for="(value, key) in row.item" :key="key">{{ key }}: {{ value}}</li>
+          <ul class="list-group">
+            <li class="list-group-item"></li>
           </ul>
         </b-card>
-      </template>
-    </b-table>
-
-    <!-- Info modal -->
-    <b-modal id="modalInfo" @hide="resetModal" :title="modalInfo.title" ok-only>
-      <pre>{{ modalInfo.content }}</pre>
-    </b-modal>
-
-  </b-container>
-    </div>
+      </b-collapse>
+    </b-container>
+  </div>
 </template>
 
 <script>
-    //import json from 'Dateien/process.json'
-    const items = [
-  { isActive: true, age: 40, name: { first: 'Dickerson', last: 'Macdonald' } },
-  { isActive: false, age: 21, name: { first: 'Larsen', last: 'Shaw' } },
-  {
-    isActive: false,
-    age: 9,
-    name: { first: 'Mini', last: 'Navarro' },
-    _rowVariant: 'success'
-  },
-  { isActive: false, age: 89, name: { first: 'Geneva', last: 'Wilson' } },
-  { isActive: true, age: 38, name: { first: 'Jami', last: 'Carney' } },
-  { isActive: false, age: 27, name: { first: 'Essie', last: 'Dunlap' } },
-  { isActive: true, age: 40, name: { first: 'Thor', last: 'Macdonald' } },
-  {
-    isActive: true,
-    age: 87,
-    name: { first: 'Larsen', last: 'Shaw' },
-    _cellVariants: { age: 'danger', isActive: 'warning' }
-  },
-  { isActive: false, age: 26, name: { first: 'Mitzi', last: 'Navarro' } },
-  { isActive: false, age: 22, name: { first: 'Genevieve', last: 'Wilson' } },
-  { isActive: true, age: 38, name: { first: 'John', last: 'Carney' } },
-  { isActive: false, age: 29, name: { first: 'Dick', last: 'Dunlap' } }
-]
-    export default {
-        data () {
-    return {
-      items: items,
-      fields: [
-        { key: 'name', label: 'Person Full name', sortable: true },
-        { key: 'age', label: 'Person age', sortable: true, 'class': 'text-center' },
-        { key: 'isActive', label: 'is Active' },
-        { key: 'actions', label: 'Actions' }
-      ],
-      currentPage: 1,
-      perPage: 5,
-      totalRows: items.length,
-      pageOptions: [ 5, 10, 15 ],
-      sortBy: null,
-      sortDesc: false,
-      filter: null,
-      striped: true,
-      modalInfo: { title: '', content: '' }
-    }
-  },
-  computed: {
-    sortOptions () {
-      // Create an options list from our fields
-      return this.fields
-        .filter(f => f.sortable)
-        .map(f => { return { text: f.label, value: f.key } })
-    }
-  },
-  methods: {
-    info (item, index, button) {
-      this.modalInfo.title = `Row index: ${index}`
-      this.modalInfo.content = JSON.stringify(item, null, 2)
-      this.$root.$emit('bv::show::modal', 'modalInfo', button)
+import json from './Dateien/process.json';
+  export default {
+    data () {
+      return {
+        details: true,
+        p: "success",
+        json: json
+      }
     },
-    resetModal () {
-      this.modalInfo.title = ''
-      this.modalInfo.content = ''
+
+    computed: {
+
     },
-    onFiltered (filteredItems) {
-      // Trigger pagination to update the number of buttons/pages due to filtering
-      this.totalRows = filteredItems.length
-      this.currentPage = 1
+
+    methods: {
+
     }
   }
-    }
 </script>
 
 <style>
-    .bg-bootstrap {
-        background-color: rgb(86, 61, 124);
-    }
+    
 </style>
