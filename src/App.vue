abf31cc1ea1f2432120672d11914a0d370227a26
DrakeProgt <nils.ziem@web.de>
2018-01-30 20:03:48 +0100

    aktualisiert

--- src/App.vue
+++ src/App.vue
@@ -6,38 +6,89 @@
         <b-navbar-brand><img src="Dateien/logo.png" class="d-inline-block align-top"> PBO-Beleg</b-navbar-brand>
       </b-container>
     </b-navbar>
-
+    <canvas id="myChart" style="width:100px; height:100px"></canvas>
     <b-container>
-      <b-btn class="btn-block mt-5" :variant='p' v-b-toggle.collapse1 @click.stop="details=!details; p=p=='primary'?'success':'primary'">Stakeholder {{ !details ? 'Zuklappen' : 'Ausklappen' }}</b-btn>
-      <b-collapse id="collapse1" class="mt-3">
-        <b-card no-body border-variant="dark" header-bg-variant="dark" header-text-variant="white">
-          <b-form-checkbox slot="header" v-model="anzeigen">Einfärben</b-form-checkbox>
-          <b-list-group flush>
-            <b-list-group-item v-for="item in json.process.stakeholder" :key="item" :variant='anzeigen ? caseInit(countInit(item.id)) : ""'>
-              <h4 class="list-group-item-heading">{{item.name}}</h4>
-              <p class="list-group-item-text projekte">Hat {{countInit(item.id)}} Projekte</p>
-            </b-list-group-item>
-          </b-list-group>
-        </b-card>
-      </b-collapse>
+      <b-table striped hover :items="stakeholder" :fields="fields" outlined></b-table>
     </b-container>
   </div>
 </template>
 
 <script>
 import JSON from '../Dateien/process.json';
+
   export default {
+  name: "app",
     data () {
       return {
-        details: true,
-        p: "success",
-        anzeigen: false,
-        name: null,
-        projekte: null,
         json: JSON,
+        fields: [
+        {
+          key: 'name',
+          label: 'Name',
+          sortable: true
+        },
+        {
+          key: 'projekt',
+          label: 'Anzahl der initiierten Projekte',
+          sortable: true
+        },
+        {
+          key: 'details',
+          label: 'Zeige Details'
+        }
+      ],
+        stakeholder:[],
       }
     },
+    beforeMount(){
+      this.fillArray();
+    },
+      mounted() {
+          var ctx = document.getElementById("myChart");
+          var myChart = new Chart(ctx, {
+          type: 'bar',
+          data: {
+            labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+            datasets: [{
+                label: '# of Votes',
+                data: [12, 19, 3, 5, 2, 3],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255,99,132,1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero:true
+                    }
+                }]
+            }
+        }
+    });
+   },
     methods: {
+      fillArray: function(){
+        var vm=this;
+        JSON.process.stakeholder.forEach(function(child){
+          vm.stakeholder.push({name: child.name, projekt: vm.countInit(child.id)});
+        });
+      },
       countInit: function(name){
         var count=0;
         JSON.process.childs.forEach(function(child){
@@ -46,21 +97,6 @@ import JSON from '../Dateien/process.json';
           }
         });
         return count;
-      },
-      caseInit(zahl){
-        switch(zahl){
-          case 0:
-            return "primary";
-            break;
-          case 1: case 2: case 3: case 4: case 5:
-            return "success";
-            break;
-          case 6: case 7: case 8: case 9: case 10:
-            return "warning";
-            break;
-          default:
-            return "danger";  
-        }
       }
     }
   }
