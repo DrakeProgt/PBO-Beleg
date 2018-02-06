b8da88db9d28950b104c31a34e98f27faec0843a
DrakeProgt <nils.ziem@web.de>
2018-02-06 04:19:13 +0100

    aktualisiert

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,64 @@
 <template>
   <div>
-    <!-- Navbar -->
+
     <b-navbar variant="dark" type="dark">
       <b-container>
         <b-navbar-brand><img src="Dateien/logo.png" class="d-inline-block align-top"> PBO-Beleg</b-navbar-brand>
       </b-container>
     </b-navbar>
 
-    <canvas id="myChart"></canvas>
-
     <b-container>
-      <b-table striped hover :items="stakeholder" :fields="fields" outlined>
+      <b-row>
+      <b-col md="6" class="my-4">
+        <b-form-group horizontal label="Filter:" class="mb-0">
+          <b-input-group>
+            <b-form-input v-model="filter" placeholder="Suchbegriff" />
+            <b-input-group-button>
+              <b-btn :disabled="!filter" @click="filter = ''">Säubern</b-btn>
+            </b-input-group-button>
+          </b-input-group>
+        </b-form-group>
+      </b-col>
+      <b-col md="6" class="my-4">
+        <b-form-group horizontal label="Sortierung:" class="mb-0">
+          <b-input-group>
+            <b-form-select v-model="sortBy" :options="sortOptions">
+              <option slot="first" :value="null">keine</option>
+            </b-form-select>
+            <b-input-group-button>
+              <b-form-select :disabled="!sortBy" v-model="sortDesc">
+                <option :value="false">Asc</option>
+                <option :value="true">Desc</option>
+              </b-form-select>
+            </b-input-group-button>
+          </b-input-group>
+        </b-form-group>
+      </b-col>
+      <b-col md="6" class="my-2">
+        <b-pagination :total-rows="totalRows" :per-page="perPage" v-model="currentPage" class="my-0" />
+      </b-col>
+      <b-col md="6" class="my-2">
+        <b-form-group horizontal label="pro Seite:" class="mb-4">
+          <b-form-select :options="pageOptions" v-model="perPage" />
+        </b-form-group>
+      </b-col>
+    </b-row>
+
+    <b-table 
+      striped 
+      hover 
+      fixed
+      :items="stakeholder" 
+      :fields="fields" 
+      outlined 
+      show-empty 
+      :current-page="currentPage"
+      :per-page="perPage"
+      :filter="filter"
+      :sort-by.sync="sortBy"
+      :sort-desc.sync="sortDesc"
+      @filtered="onFiltered">
+
         <template slot="details" slot-scope="row">      
           <b-button size="sm" @click.stop="row.toggleDetails">
             {{ row.detailsShowing ? 'Weniger' : 'Mehr'}} Details
@@ -38,6 +86,7 @@
         </template>
       </b-table>
     </b-container>
+
   </div>
 </template>
 
@@ -50,38 +99,82 @@ import JSON from '../Dateien/process.json';
       return {
         json: JSON,
         fields: [
-        {
-          key: 'name',
-          label: 'Name',
-          sortable: true
-        },
-        {
-          key: 'projekt',
-          label: 'Anzahl der initiierten Events',
-          sortable: true
-        },
-        {
-          key: 'details',
-          label: 'Details'
-        }
-      ],
+          {
+            key: 'name',
+            label: 'Name',
+            sortable: true
+          },
+          {
+            key: 'projekt',
+            label: 'Anzahl der initiierten Events',
+            sortable: true
+          },
+          {
+            key: 'details',
+            label: 'Details'
+          }
+        ],
         stakeholder:[],
+        years:[],
+        currentPage: 1,
+        perPage: 5,
+        totalRows: 0,
+        pageOptions: [ 5, 10, 15, 20 ],
+        sortBy: null,
+        sortDesc: false,
+        filter: null
       }
     },
     beforeMount(){
       this.fillArray();
+      this.totalRows=this.stakeholder.length;
     },
     mounted() {      
-        var myChart = "myChart";
+        
+    },
+    computed: {
+      sortOptions () {
+        return this.fields
+          .filter(f => f.sortable)
+          .map(f => { return { text: f.label, value: f.key } })
+       }
+    },
+    methods: {
+      fillArray: function(){
+        var vm=this;        
+        JSON.process.stakeholder.forEach(function(child){
+          vm.stakeholder.push({name: child.name, projekt: vm.countInit(child.id), namen: vm.collectNames(child.id), finish: vm.countEnd(child.id), part: vm.part(child.id)});
+        });
+        JSON.process.childs.forEach(function(child){
+          if(!vm.years.includes(child.start.substr(0,4))){
+            vm.years.push(child.start.substr(0,4));
+          }
+        });
+        vm.years.sort();        
+      },
+      chart: function(zahl){
+        var yearsCounter=[];
+        var vm = this;
+        var count=0;
+        vm.years.forEach(function(year){
+          count=0
+          JSON.process.childs.forEach(function(child){
+            if(year==child.start.substr(0,4) && child.initiator==JSON.process.stakeholder[zahl].id){
+              count++;
+            }
+          });
+          yearsCounter.push(count);
+        });
+        var myChart = "myChart"+String(zahl);
         var ctx = document.getElementById(myChart);
         var myChart = new Chart(ctx, {
           responsive: true,
           type: 'line',
           data: {
-            labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+            labels: this.years,
             datasets: [{
               label: 'Anzahl',
-              data: [12, 19, 3, 5, 2, 3]           
+              data: yearsCounter    
             }]
           },
           options: {
@@ -99,7 +192,6 @@ import JSON from '../Dateien/process.json';
                 pointStyle: 'rect',
                 backgroundColor: 'rgba(255, 153, 51, 1)',
                 borderColor: 'rgba(255, 153, 51, 1)',
-                HighlightFill: 'rgba(25, 15, 51, 1)',
                 radius: 5,
                 hoverRadius: 8
               },
@@ -113,72 +205,28 @@ import JSON from '../Dateien/process.json';
             legend: {
               display: false
             },
-            layout: {
-              padding: 20
-            },
             scales: {
               yAxes: [{
                 scaleLabel: {
-                  fontColor: '#123'
+                  display: true,
+                  labelString: 'Anzahl der initiierten Events',
+                  fontColor: '#000',
+                  fontStyle: 'bold',
+                  fontSize: 14
                 },
                 ticks: {
                   beginAtZero: true,
                   stepSize: 1,
-                  max: 20
+                  max: Math.max.apply(null, yearsCounter)+1
                 }
               }],
               xAxes: [{
                 scaleLabel: {
-                  fontColor: "green"
-                }
-              }]
-            }
-          }
-        });
-    },
-    methods: {
-      fillArray: function(){
-        var vm=this;        
-        JSON.process.stakeholder.forEach(function(child){
-          vm.stakeholder.push({name: child.name, projekt: vm.countInit(child.id), namen: vm.collectNames(child.id), finish: vm.countEnd(child.id), part: vm.part(child.id)});
-        });
-      },
-      chart: function(zahl){
-        var myChart = "myChart"+String(zahl);
-        var ctx = document.getElementById(myChart);
-        var myChart = new Chart(ctx, {
-          responsive: true,
-          type: 'line',
-          data: {
-            labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-            datasets: [{
-              label: '# of Votes',
-              data: [12, 19, 3, 5, 2, 3],
-              backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-              ],
-              borderColor: [
-                'rgba(255,99,132,1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-              ],
-              borderWidth: 1
-            }]
-          },
-          options: {
-            scales: {
-              yAxes: [{
-                ticks: {
-                  beginAtZero:true,
-                  stepSize: 1
+                  display: true,
+                  labelString: 'Jahre',
+                  fontColor: '#000',
+                  fontStyle: 'bold',
+                  fontSize: 14
                 }
               }]
             }
@@ -241,6 +289,13 @@ import JSON from '../Dateien/process.json';
         }
 
         return part;
+      },
+      onFiltered (filteredItems) {      
+        this.totalRows = filteredItems.length;
+        this.currentPage = 1;
+      },
+      lengthStake: function(){
+        return stakeholder.length();
       }
     }
   }
